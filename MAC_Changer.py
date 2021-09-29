1e76490aba7b94e5bf23b885be62c452e2d37aae
Geoffrey Owden <geoffrey.owden@ibm.com>
2021-09-29 08:56:48 +0100

    MAC changer finished

--- MAC_Changer.py
+++ MAC_Changer.py
@@ -1,18 +1,17 @@
 #!/bin/bash/env python
 
 '''MAC address control changer
-
 The subprocess module will execute system commands regardless of the OS. IT will adapt to the OS.
 see https://docs.python.org/3/library/subprocess.html
 '''
 
-# en0 MAC address ether = 38:f9:d3:83:27:4b
 
 import subprocess
-import optparse                                                                     # Module for taking in arguments when running the script
+import optparse   
+import re                                                                               # Module for taking in arguments when running the script
 
 def get_arguments():
-    parserInstance = optparse.OptionParser()                                        # Create a parser object instance
+    parserInstance = optparse.OptionParser()                                            # Create a parser object instance
 
     parserInstance.add_option("-i", "--interface", 
                         dest="interface", 
@@ -36,8 +35,21 @@ def change_MAC(interface, new_mac):
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
+def get_current_MAC(interface):
+    ifconfig_result = str(subprocess.check_output(["ifconfig", interface]))                  # check_output returns the output by the command given.
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)      # Regex rules need to start with 'r'
+
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)                                            # group(0) is the first result
+    else:
+        print("[-] Could not read MAC address.")
+
 MyOptions = get_arguments()
-change_MAC(MyOptions.interface, MyOptions.new_mac)
+current_mac = get_current_MAC(MyOptions.interface)
+print("Current_mac == " + str(current_mac))
+
+# --- uncomment to implement changeing 
+# change_MAC(MyOptions.interface, MyOptions.new_mac)
 
 
 
